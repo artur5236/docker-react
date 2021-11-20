52bac079ec3c515b1ae8083df5f457057dc49ecc
artur5236 <artur.5236@gmail.com>
2021-11-20 13:40:16 +0100

    change app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My name is Ar!!!
+          Feature branch
         </p>
         <a
           className="App-link"
